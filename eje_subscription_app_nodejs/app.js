8013e63df706c2291781d43aa24f1b7858f1133b
Juan Pablo <devecchi@exxo.es>
2020-08-24 14:03:22 +0200

    add dependencies

--- eje_subscription_app_nodejs/app.js
+++ eje_subscription_app_nodejs/app.js
@@ -13,6 +13,9 @@ server.listen(port, hostname, () => {
   console.log(`El servidor se está ejecutando en http://${hostname}:${port}/`);
 });
 
+/*
+  Express
+*/
 const express = require('express');
 const app = express();
 
@@ -21,3 +24,18 @@ app.get('/ping', (request, response) => {
 });
 app.listen(8080, 'localhost');
 
+/*
+  FyleSystem (fs)
+*/
+const fs = require('fs');
+
+// Traemos el contenido las subscripciones.
+let subs = fs.readFileSync('./assets/js/data.json', 'UTF-8');
+
+// Convertimos el contenido a un array para poder trabajarlo.
+let subsArray = JSON.parse(subs);
+
+/*
+  Dinero
+*/
+const dinero = require('dinero');
