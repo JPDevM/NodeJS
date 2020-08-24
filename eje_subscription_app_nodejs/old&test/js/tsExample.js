bc108f7a1e93518621f42f8e797dd18d56550ddf
Juan Pablo <devecchi@exxo.es>
2020-08-24 13:38:47 +0200

    deleted test files

--- eje_subscription_app_nodejs/old&test/js/tsExample.js
+++
@@ -1,9 +0,0 @@
-var sumar = function (n1, n2) {
-    return n1 + n2;
-};
-console.log(sumar(10, 20));
-var saludarMascota = function (mascota) {
-    return "Hola " + mascota;
-};
-console.log(saludarMascota('perro'));
-console.log(saludarMascota('vaca'));
