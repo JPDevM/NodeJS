de922edad45dba98d0ab5f7620c7f53494276572
Juan Pablo <devecchi@exxo.es>
2020-08-27 15:51:22 +0200

    add Express

--- eje_subscription_app_nodejs/src/app.js
+++ eje_subscription_app_nodejs/src/app.js
@@ -5,16 +5,19 @@ const express = require('express');
 const app = express();
 
 app.listen(8080, 'localhost');
+app.use(express.json()); // Para que express lea los archivos Json cuando una app cliente se lo envíe
+app.use(express.urlencoded({ extended: false })); // Parsea la información proveniente de un formulario. La convierte en un objeto literal y me sirve para verlo con request.body
+app.use(express.static('../public')); // La carpeta pública va a ser un recurso estático por lo que nos va a dar acceso a ella.
 
 // Requerimos la ruta
 const mainRoutes = require('./routes/mainRoutes'); // Ruta para las View
-const subscriptionRoutes = require('./routes/subscriptionRoutes'); // Ruta para manipular las subscriptions
+const subscriptionRoutes = require('./routes/subscriptionRouters'); // Ruta para manipular las subscriptions
 
 // http://expressjs.com/en/4x/api.html#app.use
 // path: es el grupo de rutas que se va a ejecutar.
 // callback: archivo que va a administrar estas rutas.
 app.use('/', mainRoutes); // Views
-app.get('/subscrption', subscriptionRoutes); // Manipular subscripciones
+app.use('/subscriptions', subscriptionRoutes); // Manipular subscripciones
 
 /*
   FyleSystem (fs)
--- eje_subscription_app_nodejs/src/routes/subscriptionRouters.js
+++ eje_subscription_app_nodejs/src/routes/subscriptionRouters.js
@@ -1,49 +1,49 @@
-// VALIDAR: Con este archivo busco poder crear, editar, etc, la BD de Subscriptions o en su defecto los JSON
-
 // Utilizamos el método Router de express
 const express = require('express');
 const router = express.Router();
 const fs = require('fs');
 const path = require('path');
-router.use(express.json()); // Para que express lea los archivos Json cuando una app cliente se lo envíe
 
 // GET
-router.get('/subscriptions', (request, response) => {
-  // OBJETIVO:Mostrar el Json subscriptions.json
+router.get('/', (request, response) => {
+  // OBJETIVO: Mostrar el Json subscriptions.json
   // FS
   let subs = fs.readFileSync('./data/subscriptions.json', 'UTF-8');
   let subsArray = JSON.parse(subs);
 
-  request.json(/* enviar acá el valor pedido */);
+  response.json(subsArray);
 });
 
-// POST
-router.post('/subscriptions/:id', (request, response) => {
-  // OBJETIVO:leer el Json subscriptions.json y enviar el contenido solicitado po POST
+router.get('/:id', (request, response) => {
+  // OBJETIVO:leer el Json subscriptions.json y enviar el contenido solicitado por POST
   // FS
-  request.send('POST received');
   let subs = fs.readFileSync('./data/subscriptions.json', 'UTF-8');
-  let subParam = JSON.parse(subs.id[request.params]);
-
-  request.json(subParam);
+  let subArray = JSON.parse(subs);
+  // Recorrer array, busco el objeto con el Id que traigo por parámetro
+  let subParam = subArray.id[request.params];
+  response.json(subParam);
 });
 
-// PUT
-router.post('/subscriptions', (request, response) => {
-  const indexPath = path.resolve(__dirname, '../views/add_existing.htm');
-  response.sendFile(indexPath);
+router.post('/', (request, response) => {
+  response.json(request.body); // Express Necesita express.urlencoded
 });
 
-// EDIT
-router.post('/subscriptions', (request, response) => {
+// PUT || PATCH
+// Métodos para editar
+// PUT Reescribe todo el registro que se está editando
+// PATCH Reescribe solo los datos que cambian.
+router.patch('/id:', (request, response) => {
   const indexPath = path.resolve(__dirname, '../views/add_existing.htm');
   response.sendFile(indexPath);
 });
 
 // DELETE
-router.post('/subscriptions/:id', (request, response) => {
+router.delete('/:id', (request, response) => {
   let subs = fs.readFileSync('./data/subscriptions.json', 'UTF-8');
   let subParam = JSON.parse(subs.id[request.params]);
 
   // FS falta buscar el parámetro y eliminarlo
 });
+
+// Exportamos las rutas
+module.exports = router;
